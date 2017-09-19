16d41b3cfeff63c109281d3f0491b8e98e1c9595
Jay Sigbrandt <jsigbrandt@slb.com>
2017-09-19 18:12:31 +0200

    adding tests

--- lnl.sh
+++ lnl.sh
@@ -10,19 +10,33 @@ ssh qxgit@build "cd $dir; \
     git config --replace-all receive.denydeletecurrent false; \
     mv $u ${u}.moved" || true
 
-cd /tmp
+# set up the links to origin
+cd /tmp 
     rm -rf /tmp/lnl
     git clone qxgit@build:/nett/uniq/git/lnl.git
-cd /tmp/lnl
+# clean out origin
+cd /tmp/lnl 
     for b in $(git branch --all | awk -F / '!/HEAD/ && /remotes/ {print $NF}'); do echo git push origin :${b}; git push origin :${b}; done
-cd /tmp
+# clean out local
+cd /tmp 
     rm -rf /tmp/lnl
     git clone qxgit@build:/nett/uniq/git/lnl.git
-cd /tmp/lnl
+# rebuild local from scratch
+cd /tmp/lnl 
     for t in $(git tag -l); do git push origin :refs/tags/${t}; git tag -d $t; done
     [ $(ls | wc -l) -eq 0 ] || exit 1
     touch file
     git add file
+    # begin tests
+    i=0
+    echo 'set -x; git push origin experiment' > test$((++i)).sh
+    echo 'set -x; git push origin h:ready' > test$((++i)).sh
+    echo 'set -x; git co maser; git pull; git co h; git rebase master; git push origin h:ready' > test$((++i)).sh
+    echo 'set -x; git pull; git co h; git reset HEAD^; git amend; git rebase master; git push origin h:ready' > test$((++i)).sh
+    echo 'set -x; git co h; git pull --no-ff --commit --no-edit origin master; git push origin h:ready' > test$((++i)).sh
+    chmod +x test*.sh
+    git add test*.sh
+    # end tests
     git commit -am "first commit"
     for c in {a..f}; do echo $c >> file; git commit -am $c; done
     git checkout -b experiment
